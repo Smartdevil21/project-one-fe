defb80b5b5fe0991295ae7bb2363844cc8605b35
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-07-26 20:14:50 +0530

    minor changes

--- src/app/(product)/analytics/page.tsx
+++ src/app/(product)/analytics/page.tsx
@@ -1,10 +1,24 @@
 // Main page for analytics
 "use client";
 
+import Heading1 from "@/components/fonts/Heading1";
+import Heading2 from "@/components/fonts/Heading2";
+// import Analytics from "@/components/product/analytics/analytics";
+import Analytics from "@/components/product/analytics/Analytics";
 import styles from "@/styles/app/product/analytics.module.scss";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.main_wrapper}>
+        <Heading1>Analytics</Heading1>
+        <Heading2>Wednesday, 19 Jul 2023</Heading2>
+        <div className={styles.main_graph}>
+          <Analytics />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default page;
---
+++ src/components/product/analytics/Analytics.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "@/styles/components/analytics/analytics.module.scss";
+import Heading3 from "@/components/fonts/Heading3";
+
+function analytics() {
+  return (
+    <div className={styles.main}>
+      <Heading3>Graph 1</Heading3>
+    </div>
+  );
+}
+
+export default analytics;
