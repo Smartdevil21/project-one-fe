2c3629dfd060d61c5d54d84ef885b1f9118d723f
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-07-26 20:27:22 +0530

    radarchart

--- src/components/product/analytics/Analytics.tsx
+++ src/components/product/analytics/Analytics.tsx
@@ -1,11 +1,80 @@
 import React from "react";
 import styles from "@/styles/components/analytics/analytics.module.scss";
 import Heading3 from "@/components/fonts/Heading3";
+import {
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Radar,
+  Legend,
+} from "recharts";
+
+const data = [
+  {
+    subject: "Math",
+    A: 120,
+    B: 110,
+    fullMark: 150,
+  },
+  {
+    subject: "Chinese",
+    A: 98,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: "English",
+    A: 86,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: "Geography",
+    A: 99,
+    B: 100,
+    fullMark: 150,
+  },
+  {
+    subject: "Physics",
+    A: 85,
+    B: 90,
+    fullMark: 150,
+  },
+  {
+    subject: "History",
+    A: 65,
+    B: 85,
+    fullMark: 150,
+  },
+];
 
 function analytics() {
   return (
     <div className={styles.main}>
       <Heading3>Graph 1</Heading3>
+      <div className={styles.graph}>
+        <RadarChart outerRadius={90} width={730} height={250} data={data}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis angle={30} domain={[0, 150]} />
+          <Radar
+            name="Mike"
+            dataKey="A"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.6}
+          />
+          <Radar
+            name="Lily"
+            dataKey="B"
+            stroke="#82ca9d"
+            fill="#82ca9d"
+            fillOpacity={0.6}
+          />
+          <Legend />
+        </RadarChart>
+      </div>
     </div>
   );
 }
